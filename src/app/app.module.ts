8c38fa8fc7e9b3003a8a4043703454b10affd5b5
Hope Tambala <hpbala@umich.edu>
2018-04-09 10:26:34 -0400

    demo of smart tables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    Ng2SmartTableModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/tables/tables.ts
+++ src/pages/tables/tables.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+
 /**
  * Generated class for the TablesPage page.
  *
@@ -10,17 +11,51 @@ import { NavController, NavParams } from 'ionic-angular';
 
 //Angular Thing
 //@IonicPage()
+
 @Component({
   selector: 'page-tables',
   templateUrl: 'tables.html',
+  
 })
 export class TablesPage {
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+  data = [
+    {
+      id: 1,
+      name: "Test Testerton",
+      DiabetesDiagnosis: "Nope",
+      Availability: "test@test.com"
+    },
+    // ... other rows here
+    {
+      id: 11,
+      name: "Tested Testme",
+      DiabetesDiagnosis: "Yep",
+      Availability: "Test.testberg@testamond.tez"
+    }
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TablesPage');
+    
   }
 
 }
